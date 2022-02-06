0affd697f12ca7e335601c76f0f20d8a604fe6d3
kretingen-yuri <98271877+kretingen-yuri@users.noreply.github.com>
2022-02-06 11:07:12 +0200

    Create config.py

---
+++ docs/source/config.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Zerto for Kubernetes'
+copyright = '2022, Zerto a Hewlett Packard Enterprise company'
+author = 'Yuri Kretingen'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {    
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
